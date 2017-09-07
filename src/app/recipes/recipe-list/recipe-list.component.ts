c7deeb9229620518a21fe6de2780084553b6d08d
Apoorva Shah <apoorvashah28@gmail.com>
2017-09-07 23:04:41 +0530

    User adding ingredient to shopping list and formatting...

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://www.jainrasoi.com/mg/wp-content/uploads/2012/09/khaman-dhokla.jpg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.tarladalal.com/members/9306/big/big_moong_dal_paratha-7987.jpg?size=696X905')
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+   this.recipeWasSelected.emit(recipe);
   }
 
 }
